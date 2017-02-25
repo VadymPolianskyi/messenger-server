c6ddb16e9e6d46d1fc35c56bbf6fe5c5b53f179f
vadym <vadym.polyanski@gmail.com>
2017-02-25 11:51:35 +0200

    added profile service

---
+++ softgroup/services/profile/profile-impl/src/main/java/com/softgroup/profile/impl/handler/ContactsSyncHandler.java
@@ -0,0 +1,19 @@
+package com.softgroup.profile.impl.handler;
+
+import com.softgroup.common.protocol.Request;
+import com.softgroup.common.protocol.Response;
+import com.softgroup.profile.api.router.*;
+/**
+ * Author: vadym
+ * Date: 25.02.17
+ * Time: 11:41
+ */
+public class ContactsSyncHandler implements ProfileRequestHandler {
+    public String getName() {
+        return null;
+    }
+
+    public Response<?> handle(Request<?> msg) {
+        return null;
+    }
+}
---
+++ softgroup/services/profile/profile-impl/src/main/java/com/softgroup/profile/impl/handler/GetOtherProfilesHandler.java
@@ -0,0 +1,20 @@
+package com.softgroup.profile.impl.handler;
+
+import com.softgroup.common.protocol.Request;
+import com.softgroup.common.protocol.Response;
+import com.softgroup.profile.api.router.ProfileRequestHandler;
+
+/**
+ * Author: vadym
+ * Date: 25.02.17
+ * Time: 11:46
+ */
+public class GetOtherProfilesHandler implements ProfileRequestHandler {
+    public String getName() {
+        return null;
+    }
+
+    public Response<?> handle(Request<?> msg) {
+        return null;
+    }
+}
