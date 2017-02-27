1c3d14270f4dbc579b70cf646b2562e4b449feec
vadym <vadym.polyanski@gmail.com>
2017-02-27 21:15:43 +0200

    added entity to db modul

---
+++ softgroup/basis/common-database/src/main/java/com/softgroup/db/DBHandler.java
@@ -0,0 +1,9 @@
+package com.softgroup.db;
+
+/**
+ * Author: vadym
+ * Date: 27.02.17
+ * Time: 19:25
+ */
+public class DBHandler {
+}
---
+++ softgroup/basis/common-database/src/main/java/com/softgroup/db/entity/data/ProfileSettings.java
@@ -0,0 +1,12 @@
+package com.softgroup.db.entity.data;
+
+import java.util.List;
+
+/**
+ * Author: vadym
+ * Date: 25.02.17
+ * Time: 10:18
+ */
+public class ProfileSettings {
+    private List<String> settings;
+}
