956c7f337d7c61ddffeb2b18f3444a38c50688ec
VadymPolyanski <vadym.polyanski@gmail.com>
2017-04-05 10:55:31 +0300

    corrected frontend service (#29)
    * corrected frontend service
    
    * renamed parameters(Request)

--- softgroup/frontend/src/main/java/com/softgroup/frontend/controller/MessengerPrivateController.java
+++ softgroup/frontend/src/main/java/com/softgroup/frontend/controller/MessengerPrivateController.java
@@ -1,4 +1,4 @@
-package com.softgroup.frontend.test.controller;
+package com.softgroup.frontend.controller;
 
 import com.softgroup.common.datamapper.DataMapper;
 import com.softgroup.common.protocol.CommonRequest;
@@ -31,8 +31,7 @@ public class MessengerPrivateController {
 
     @RequestMapping(path = "/private",
             method = RequestMethod.POST)
-    public Response privateMessenger(@RequestBody final String requestStr) {
-        CommonRequest request = jacksonDataMapper.mapData(requestStr.getBytes(StandardCharsets.UTF_8),CommonRequest.class);
+    public Response privateMessenger(@RequestBody final CommonRequest request) {
         request.setRoutingData((RoutingData) session.getAttribute("routing_data"));
         return firstRouterHandler.handle(request);
     }
