4b7e50495f531cf89f5f44d43b4c1d9564dfbf4b
VadymPolyanski <vadym.polyanski@gmail.com>
2017-04-10 18:48:29 +0300

    corrected profile service

--- softgroup/basis/common-dao/common-dao-impl/src/main/java/com/softgroup/common/dao/impl/service/ContactService.java
+++ softgroup/basis/common-dao/common-dao-impl/src/main/java/com/softgroup/common/dao/impl/service/ContactService.java
@@ -4,6 +4,9 @@ import com.softgroup.common.dao.api.entities.ContactEntity;
 import com.softgroup.common.dao.impl.repositories.ContactRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Author: vadym
  * Date: 07.03.17
@@ -11,4 +14,20 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class ContactService extends BaseService<ContactEntity, ContactRepository> {
+
+    public ContactEntity findByNameAndProfileId(String name, String profileId) {
+        return new ContactEntity();
+    }
+
+    public ContactEntity insert(ContactEntity contactEntity) {
+        return getRepository().save(contactEntity);
+    }
+
+    public void remove(ContactEntity contactEntity) {
+        getRepository().delete(contactEntity.getId());
+    }
+
+    public List<ContactEntity> findByProfileId(String profileId) {
+        return getRepository().findAll();
+    }
 }
