59b64669aed5ee3a45d10d6c85feaea8715dc723
VadymPolyanski <vadym.polyanski@gmail.com>
2017-04-11 23:50:38 +0300

    Update ContactService.java
    reverted ContactService

--- softgroup/basis/common-dao/common-dao-impl/src/main/java/com/softgroup/common/dao/impl/service/ContactService.java
+++ softgroup/basis/common-dao/common-dao-impl/src/main/java/com/softgroup/common/dao/impl/service/ContactService.java
@@ -4,9 +4,6 @@ import com.softgroup.common.dao.api.entities.ContactEntity;
 import com.softgroup.common.dao.impl.repositories.ContactRepository;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Author: vadym
  * Date: 07.03.17
@@ -14,20 +11,4 @@ import java.util.List;
  */
 @Component
 public class ContactService extends BaseService<ContactEntity, ContactRepository> {
-
-    public ContactEntity findByNameAndProfileId(String name, String profileId) {
-        return new ContactEntity();
-    }
-
-    public ContactEntity insert(ContactEntity contactEntity) {
-        return getRepository().save(contactEntity);
-    }
-
-    public void remove(ContactEntity contactEntity) {
-        getRepository().delete(contactEntity.getId());
-    }
-
-    public List<ContactEntity> findByProfileId(String profileId) {
-        return getRepository().findAll();
-    }
 }
