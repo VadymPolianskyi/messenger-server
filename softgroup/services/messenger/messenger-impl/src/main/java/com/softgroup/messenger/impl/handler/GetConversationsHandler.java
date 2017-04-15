3a1c486b57f5dde69c30df564fcf2240846baf3e
VadymPolyanski <vadym.polyanski@gmail.com>
2017-04-15 12:47:52 +0300

    started to develop second part of messenger

--- softgroup/services/messenger/messenger-impl/src/main/java/com/softgroup/messenger/impl/handler/GetConversationsHandler.java
+++ softgroup/services/messenger/messenger-impl/src/main/java/com/softgroup/messenger/impl/handler/GetConversationsHandler.java
@@ -1,12 +1,18 @@
 package com.softgroup.messenger.impl.handler;
 
+import com.softgroup.common.dao.api.entities.types.ConversationType;
 import com.softgroup.common.protocol.Request;
 import com.softgroup.common.protocol.Response;
+import com.softgroup.common.protocol.Status;
 import com.softgroup.common.router.api.AbstractRequestHandler;
 import com.softgroup.messenger.api.message.GetConversationsRequest;
 import com.softgroup.messenger.api.message.GetConversationsResponse;
 import com.softgroup.messenger.api.router.MessengerRequestHandler;
+import com.softgroup.profile.api.dto.ConversationDTO;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Author: vadym
@@ -24,6 +30,24 @@ public class GetConversationsHandler
 
     @Override
     public Response<GetConversationsResponse> doHandle(Request<GetConversationsRequest> request) {
-        return null;
+        GetConversationsRequest requestData = request.getData();
+        GetConversationsResponse getConversationsResponse =
+                new GetConversationsResponse();
+
+        String profileId = request.getRoutingData().getProfileId();
+        ConversationType conversationType = requestData.getType();
+        List<ConversationDTO> conversationDTOS = getConversationDTO(conversationType, profileId);
+
+        if (conversationDTOS.size() < conversationIds.size()) {
+            return responseFactory.createResponse(request, Status.NOT_FOUND);
+        } else {
+            getConversationByIdsResponse.setConversationDTOS(conversationDTOS);
+            return responseFactory.createResponse(request, getConversationByIdsResponse);
+        }
+    }
+
+    private List<ConversationDTO> getConversationDTO(ConversationType conversationType,
+                                                        String profileId) {
+        List<ConversationDTO> conversationDTOs = new ArrayList<ConversationDTO>();
     }
 }
